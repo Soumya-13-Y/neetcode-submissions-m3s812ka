32d16e44c7fb6eee6837d1dea334f93cc698eb1f
Soumya-13-Y <soumya19988@gmail.com>
2026-04-15 00:22:44 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        for (int p : piles) {
+            max = Math.max(max, p);
+        }
+
+        int low = 1, high = max;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (calculateTime(piles, mid) <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    int calculateTime(int[] piles, int speed) {
+        int hours = 0;
+        for (int p : piles) {
+            hours += (p + speed - 1) / speed;
+        }
+        return hours;
+    }
+}
